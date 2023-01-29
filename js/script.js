0421a14ba614d24870f62ede7c8b5d7eb8206115
Saraesp <saraesposito145@gmail.com>
2023-01-29 22:33:33 +0100

    creazione button

--- js/script.js
+++ js/script.js
@@ -5,13 +5,50 @@ createApp({
         return{
             newTask: '',
             todolist: [
-                'Fare la spesa',
-                'Portare a spasso il cane',
-                'Preparare la cena',
-                'Riparare il Frigo',
-                'Lavare la macchina',
-                'fare l\'esercizio di Boolean',
+                {
+                    text: 'Fare la spesa',
+                    done: false,
+                },
+                {
+                    text: 'Portare a spasso il cane',
+                    done: false,
+                },
+                {
+                    text: 'Preparare la cena',
+                    done: false,
+                },
+                {   
+                    text: 'Riparare il Frigo',
+                    done: false,
+                },
+                {
+                    text: 'Lavare la macchina',
+                    done: false,
+                },
+                {
+                    text: 'Fare l\'esercizio di Boolean',
+                    done: false,
+                }
             ]
+        }
+    },
+    methods: {
+        addTask(){
+            this.todolist.push(this.newTask);
+            this.newTask = '';
+        },
+        removeTask(index){
+            this.todolist.splice(index, 1);
+        },
+        checkTask(index){
+            let task = this.todolist[index];
+
+            if(task.done == true){
+                task.done = false;
+            }
+            else{
+                task.done = true;
+            }
         }
     }
 }).mount('#app');
