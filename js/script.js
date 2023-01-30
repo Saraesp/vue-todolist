ed2196d09680617152508ebdd470ab349e9cfcd0
Saraesp <saraesposito145@gmail.com>
2023-01-30 21:20:51 +0100

    barra di ricerca e impostazione stile

--- js/script.js
+++ js/script.js
@@ -4,37 +4,65 @@ createApp({
     data(){
         return{
             newTask: '',
+            search: '',
             todolist: [
                 {
                     text: 'Fare la spesa',
                     done: false,
+                    edit: false,
                 },
                 {
                     text: 'Portare a spasso il cane',
                     done: false,
+                    edit: false,
                 },
                 {
                     text: 'Preparare la cena',
                     done: false,
+                    edit: false,
                 },
                 {   
                     text: 'Riparare il Frigo',
                     done: false,
+                    edit: false,
                 },
                 {
                     text: 'Lavare la macchina',
                     done: false,
+                    edit: false,
                 },
                 {
                     text: 'Fare l\'esercizio di Boolean',
                     done: false,
+                    edit: false,
                 }
             ]
         }
     },
+    computed:{
+        searchTask(){
+
+            let filteredTask;
+            if(this.search != ''){
+                filteredTask = this.todolist.filter((elem) => {
+                    return elem.text.toLowerCase().includes(this.search).toLowerCase();
+                })
+            }else{
+                filteredTask = this.todolist;
+            }
+            
+            return filteredTask
+        }
+        
+    },
     methods: {
         addTask(){
-            this.todolist.push(this.newTask);
+            let object = {
+                text: this.newTask,
+                done: false
+            }
+
+            this.todolist.push(object);
             this.newTask = '';
         },
         removeTask(index){
@@ -49,6 +77,22 @@ createApp({
             else{
                 task.done = true;
             }
+        },
+        showEdit(index){
+            let task = this.todolist[index];
+
+            if(task.edit == true){
+
+                if(task.text == ''){
+                    this.removeTask(index);
+                }
+
+                task.edit = false;
+            }
+            else{
+                task.edit = true;
+            }
         }
+
     }
 }).mount('#app');
